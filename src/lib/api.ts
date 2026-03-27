a0a00be5b88d4a3e0d31e2811c70eda86fd43ec8
himanshusingh2907 <captaincool6166@gmail.com>
2026-03-28 04:05:13 +0530

    fix: use env variable for API base URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios'
 import type { AxiosInstance, AxiosRequestConfig } from 'axios'
 
-const API_BASE_URL = 'https://docmind-backend-3wlu.onrender.com/api'
+const API_BASE_URL =  import.meta.env.VITE_API_URL || 'http://localhost:5000'
  //'http://localhost:5000/api'
 
 type RetryConfig = AxiosRequestConfig & { _retry?: boolean }
